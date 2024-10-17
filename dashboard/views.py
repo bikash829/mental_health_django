3ae130f9c480faa5ac6f223417baf973ff262f42
Bikash Chowdury <chowdhurybikash38@gmail.com>
2024-10-17 23:27:52 +0600

    layouting

---
+++ dashboard/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def dashboard(request):
+
+    template_name = "dashboard/layouts/base.html"
+    context = {
+
+    }
+    return render(request,template_name,context)
