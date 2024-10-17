31f8d65d5f97ab8737a5bedade8b4450dc475186
Bikash Chowdury <chowdhurybikash38@gmail.com>
2024-10-17 22:42:33 +0600

    models created

--- accounts/models.py
+++ accounts/models.py
@@ -20,7 +20,17 @@ def identity_type_directory_path(instance, filename):
 def profile_photo_directory_path(instance, filename):
     return generate_upload_path(instance, filename, 'profile')
 
+# education certificate 
+def education_certificate_directory_path(instance, filename):
+    return generate_upload_path(instance, filename, 'employee/education_certificates')
+
+# training certificate 
+def training_certificate_directory_path(instance, filename):
+    return generate_upload_path(instance, filename, 'employee/training_certificates')
+
 # Create your models here.
+
+# Blood group model
 class BloodGroup(models.Model):
     group = models.CharField(max_length=10)
 
@@ -28,7 +38,7 @@ class BloodGroup(models.Model):
     def __str__(self):
         return self.group
 
-
+# User model description 
 class User(AbstractUser):
     """ choices """
     # marital status
@@ -67,14 +77,72 @@ class User(AbstractUser):
     phone = models.CharField(max_length=30)
     additional_phone_code = models.CharField(max_length=10)
     additional_phone = models.CharField(max_length=30)
-    date_of_birth = models.DateField(null=True,blank=True)
+    date_of_birth = models.DateField(null=True)
     religion = models.CharField(max_length=50)
-    identity_type = models.IntegerField(choices=IdentityType,default=0)
+    identity_type = models.IntegerField(choices=IdentityType,null=True)
     identity_no = models.CharField(max_length=100)
     identity_proof = models.FileField(upload_to=identity_type_directory_path)
     profile_photo = models.ImageField(upload_to=profile_photo_directory_path)
-    is_verified = models.IntegerField(choices=IS_VERIFIED,default=2)
+    is_verified = models.IntegerField(choices=IS_VERIFIED,null=True)
     blood_group = models.ForeignKey(BloodGroup,on_delete=models.PROTECT,null=True)
     terms = models.BooleanField(default=0)
 
+
+# Address info description 
+class Address(models.Model):
+    address = models.CharField(max_length=200)
+    address2 = models.CharField(max_length=200,null=True,blank=True)
+    zip_code = models.CharField(max_length=10)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.user
+    
+
+# Education info description
+class Education(models.Model):
+    institute = models.CharField(max_length=200)
+    specialization = models.CharField(max_length=100)
+    duration = models.CharField(max_length=20)
+    passing_year = models.DateField()
+    certificate = models.FileField(upload_to=education_certificate_directory_path)
+    certificate_title = models.CharField(max_length=200)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.institute
+    
+
+# Experience info description 
+class Experience(models.Model):
+    org_name  = models.CharField(max_length=200,verbose_name='Organization Name')
+    department = models.CharField(max_length=100)
+    from_date = models.DateField()
+    to_date = models.DateField(null=True, blank=True)
+    job_status = models.CharField(max_length=10,null=True,blank=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    
+    def __str__(self):
+        return self.org_name
+
+
+# Training info model description 
+class Training(models.Model):
+    institute = models.CharField(max_length=200)
+    specialization = models.CharField(max_length=100)
+    from_date = models.DateField()
+    to_date = models.DateField()
+    training_title = models.CharField(max_length=200)
+    training_certificate = models.FileField(upload_to=training_certificate_directory_path)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.institute
 
