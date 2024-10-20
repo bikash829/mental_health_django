074396ed4d8c18b60d44317f608432fbee64397a
Bikash Chowdury <chowdhurybikash38@gmail.com>
2024-10-20 18:28:22 +0600

    intigrate main app

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 
 from .models import User,BloodGroup, Education, Address, Training, Experience
 # Register your models here.
@@ -30,14 +32,32 @@ class UserGroupInline(admin.TabularInline):
     extra = 1
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+
     list_display = ["username", "email","gender","date_of_birth","is_verified","get_groups","is_superuser"]
 
+    # fields = ['username','first_name','last_name','email','marital_status','gender','phone_code','phone',
+    #           'additional_phone_code','additional_phone','date_of_birth','religion','identity_type','identity_no','identity_proof',
+    #           'profile_photo','is_verified','blood_group','terms','is_staff']
 
+    # fieldsets = (
+    #     (None, {'fields': ('username', 'password')}),
+    #     ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'marital_status', 'gender', 'phone_code', 'phone',
+    #                                   'additional_phone_code', 'additional_phone', 'date_of_birth', 'religion', 'identity_type', 'identity_no', 'identity_proof',
+    #                                   'profile_photo', 'blood_group', 'terms')}),
+    #     ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+    #     ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    # )
 
-    fields = ['username','first_name','last_name','email','password','marital_status','gender','phone_code','phone',
-              'additional_phone_code','additional_phone','date_of_birth','religion','identity_type','identity_no','identity_proof',
-              'profile_photo','is_verified','blood_group','terms','is_staff']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'email', 'first_name', 'last_name', 'gender', 'date_of_birth', 'is_verified', 'is_staff', 'is_active', 'is_superuser', )}
+            # 'fields': ('username', 'password1', 'password2', 'email', 'first_name', 'last_name', 'gender', 'date_of_birth', 'is_verified', 'is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}
+        ),
+    )
     
     
     inlines = [
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
-
+app_name = 'accounts'
 urlpatterns = [
-    
+    path("", include("django.contrib.auth.urls")),
+    # path('sign_up/', views.sign_up,name="sign_up"),
+    # path('profile/',views.profile,name="profile"),
 ]
