295492fc62acf4733db4e66d625249bd83d21692
Bikash Chowdury <chowdhurybikash38@gmail.com>
2024-11-09 10:47:48 +0600

    Enhance password change functionality with improved templates and URL routing

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -5,13 +5,24 @@ from django.urls import reverse_lazy
 
 app_name = 'accounts'
 urlpatterns = [
+    path(
+        "password_change/", auth_views.PasswordChangeView.as_view(
+            success_url=reverse_lazy("accounts:password_change_done")
+        ), name="password_change"
+    ),
     path('password_reset/', auth_views.PasswordResetView.as_view(
         success_url=reverse_lazy('accounts:password_reset_done')), name='password_reset'), # must maintain order
-    # path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-    #     success_url=reverse_lazy('accounts:password_reset_complete')), name='password_reset_confirm'),
     path("", include("django.contrib.auth.urls")),
     path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
         success_url=reverse_lazy('accounts:password_reset_complete')), name='password_reset_confirm'),
+    # path(
+    #     "password_change/done/",
+    #     auth_views.PasswordChangeView.as_view(
+    #         success_url=reverse_lazy("accounts:password_change_done")
+    #     ),
+    #     name="password_change_done",
+    # ),
+    
         
     path('profile/',views.profile,name="profile"),
     path('change_profile_photo/',views.change_profile_photo,name='change_profile_photo'),
